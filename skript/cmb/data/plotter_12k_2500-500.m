92382b045878c89956015415832e4b427f5d88f5
Hansruedi Patzen <hpatzen@hsr.ch>
2017-06-20 16:05:56 +0200

    added Cl calc

--- skript/cmb/data/plotter_12k_2500-500.m
+++ skript/cmb/data/plotter_12k_2500-500.m
@@ -23,8 +23,8 @@ p = polyfit(xmin:xmax, C(xmin:xmax), order);
 x = linspace(xmin, xmax, xmax - xmin + 1);
 y = polyval(p, x);
 
-xlabel("l");
-ylabel("l(l+1)C_l/2{/Symbol p} -> skaliert");
+xlabel("Multipole moment l");
+ylabel("l(l+1)C_l/2{/Symbol p}, scaled");
 title("CMB Leistungsspektrum");
 
 hold on;
@@ -46,7 +46,7 @@ endfor
 plot(xmin:plotmax, C(xmin:plotmax));
 %plot([180, 180], [ymin,ymax], 'r');
 set (0, "defaultlinelinewidth", 8);
-plot(x(1:plotmax),y(1:plotmax), ";data best fit ;");
+plot(x(1:plotmax),y(1:plotmax), ";Data best fit ;");
 
 %set(gca, 'xtick', [10 50 100 200 500 1000 2000]);
 %set(gca, 'xticklabel', [10 50 100 200 500 1000 2000]);
--- skript/cmb/data/plotter_2k_900-500.m
+++ skript/cmb/data/plotter_2k_900-500.m
@@ -21,8 +21,8 @@ p = polyfit(xmin:xmax, C(xmin:xmax), order);
 x = linspace(xmin, xmax, xmax - xmin + 1);
 y = polyval(p, x);
 
-xlabel("l");
-ylabel("l(l+1)C_l/2{/Symbol p} -> skaliert");
+xlabel("Multipole moment l");
+ylabel("l(l+1)C_l/2{/Symbol p}, scaled");
 title("CMB Leistungsspektrum");
 
 hold on;
@@ -43,7 +43,7 @@ endfor
 plot(xmin:xmax, C(xmin:xmax));
 %plot([180, 180], [ymin,ymax], 'r');
 set (0, "defaultlinelinewidth", 8);
-plot(x,y, ";data best fit ;");
+plot(x,y, ";Data best fit ;");
 
 %set(gca, 'xscale', 'log');
 %set(gca, 'xtick', [10 50 100 200 300 500 1000 2500]);
