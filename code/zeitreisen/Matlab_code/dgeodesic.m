d9be83ef898b8eb424beeefc3f2a79fed2212204
Sascha Jecklin <jecklin94@gmail.com>
2017-07-06 08:54:47 -0400

    clean up

--- code/zeitreisen/Matlab_code/dgeodesic.m
+++ code/zeitreisen/Matlab_code/dgeodesic.m
@@ -3,15 +3,16 @@
 %
 
 function [ y ] = dgeodesic( s,x )
-n = floor(size(x,1)/2);    %size(x) should be even anyway
-y = zeros(2*n,1);
-p = position(x);
-u = velocity(x);
-y(1:n,1) = u;
-for alpha = 1:n
-    Gamma = christoffel(p, alpha);
-    y(n+alpha,1) = -u' * Gamma * u;
-end
+    n = floor(size(x,1)/2);    %size(x) should be even anyway
+    y = zeros(2*n,1);
+    p = position(x);
+    u = velocity(x);
+    y(1:n,1) = u;
+
+    for alpha = 1:n
+        Gamma = christoffel(p, alpha);
+        y(n+alpha,1) = -u' * Gamma * u;
+    end
 
 end
 
